92deadfebc851b30f5f81fdb444dedbc7a36af7f
Prabesh Rai <raiprabesh789@gmail.com>
2023-10-25 22:06:05 +0545

    python: float

--- calculator.py
+++ calculator.py
@@ -8,3 +8,12 @@ x = float(input("What's x: "))
 y = float(input("what's y: "))
 z = round(x + y)
 print(f"{z:,}")  # number format using commas as separator = 1,000
+
+x = float(input("What's x: "))
+y = float(input("what's y: "))
+z = round(x / y, 2)
+print(z)
+
+#
+z = (x / y)
+print(f"{z:.2f}")  # f string approach
