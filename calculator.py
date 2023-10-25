a06ad46f4f6f3463d4db991b4addcc42d901b970
Prabesh Rai <raiprabesh789@gmail.com>
2023-10-25 19:50:05 +0545

    python: float

--- calculator.py
+++ calculator.py
@@ -2,3 +2,9 @@
 x = int(input("What's x: "))
 y = int(input("what's y: "))
 print(x + y)
+
+#float
+x = float(input("What's x: "))
+y = float(input("what's y: "))
+z = round(x + y)
+print(f"{z:,}")  # number format using commas as separator = 1,000
