d6e3bdf9aa4d40cc35a386b9621b48eccef45e6b
Prabesh Rai <raiprabesh789@gmail.com>
2023-10-26 21:49:09 +0545

    python: retrun value

--- calculator.py
+++ calculator.py
@@ -1,19 +1,31 @@
 #int
-x = int(input("What's x: "))
-y = int(input("what's y: "))
-print(x + y)
+# x = int(input("What's x: "))
+# y = int(input("what's y: "))
+# print(x + y)
 
-#float
-x = float(input("What's x: "))
-y = float(input("what's y: "))
-z = round(x + y)
-print(f"{z:,}")  # number format using commas as separator = 1,000
+# #float
+# x = float(input("What's x: "))
+# y = float(input("what's y: "))
+# z = round(x + y)
+# print(f"{z:,}")  # number format using commas as separator = 1,000
 
-x = float(input("What's x: "))
-y = float(input("what's y: "))
-z = round(x / y, 2)
-print(z)
+# x = float(input("What's x: "))
+# y = float(input("what's y: "))
+# z = round(x / y, 2)
+# print(z)
 
-#
-z = (x / y)
-print(f"{z:.2f}")  # f string approach
+# #
+# z = (x / y)
+# print(f"{z:.2f}")  # f string approach
+
+
+def main():
+    x = int(input("What's X ? "))
+    print("X squared is", square(x))
+
+
+def square(n):
+    return n * n # n ** 2 # pow(n, 2)
+
+
+main()
