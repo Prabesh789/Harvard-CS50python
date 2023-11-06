29a38a236e733f658235a594d75bbe0868c490b9
Prabesh Rai <raiprabesh789@gmail.com>
2023-11-06 09:34:24 +0545

    python: unit test

--- unit_test/test_calculator.py
+++ unit_test/test_calculator.py
@@ -1,14 +1,14 @@
+import pytest
+
 from calculate import square
-
-
+'''
 def test_square():
     assert square(2) == 4
     assert square(3) == 9
     assert square(-2) == 4
     assert square(-3) == 9
     assert square(0) == 0
-
-
+'''
 '''
 This whole test is automate by pytest library
 
@@ -32,3 +32,24 @@ run cmd pytest test_calculator.py
 # if __name__ == "__main__":
 #     main()
 '''
+
+
+# spearating big test to multiple test
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
+    assert square(0) == 0
+
+
+# test for string input, here square function is not wrong but there is type error
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
