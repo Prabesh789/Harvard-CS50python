dd6d7c6ea799705b382c54a7e081bb38cdea88d1
Prabesh Rai <raiprabesh789@gmail.com>
2023-11-19 10:05:52 +0545

    py: def of class

---
+++ oop/student2.py
@@ -0,0 +1,13 @@
+"""
+Python allow us many possible types of data types to store the data. Also python allow us 
+another general purpose tool that's going to allow us to create our own data types as well
+and actually give them names and that terminology is a Class.
+
+Class: it is kind of like a blueprint for pieces of data, object so to speak. We can define
+and give them a name and when we use that mold or blueprint we get types of data that are 
+designed exactly as we want so in short classes allow us to invent our own data types in Python 
+and give them a name.
+
+This is the primary feature of object oriented progamming to be able to create our own objects 
+with own custom names.
+"""
