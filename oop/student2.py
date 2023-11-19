85342ec9bafa3a88c412cb7b8e825f5da62045c0
Prabesh Rai <raiprabesh789@gmail.com>
2023-11-19 10:30:06 +0545

    py: class in python

--- oop/student2.py
+++ oop/student2.py
@@ -10,4 +10,37 @@ and give them a name.
 
 This is the primary feature of object oriented progamming to be able to create our own objects 
 with own custom names.
+
+Python classes allow for the creation of objects that can have both attributes (data) and methods (functions).
+Attributes are defined within a class and can be accessed and modified by both the class and its objects.
+"""
+
+
+# new data type Student
+class Student():
+    ...
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
+
+
+if __name__ == "__main__":
+    main()
+
+    #
+    #
+""" 
+Here "student" in line 30 is an object of class Student or instantiation / instance of class Student.
+An object that is created using a class is said to be an instance of that class. We will sometimes say
+that the object belongs to the class. The variables that the object contains are called instance variables.
+Here, in student.name and student.home in line 31 & 32 name & house are instance variables or attributes.
 """
