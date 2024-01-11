7d090dffb9f8bffbc89a5b64a8936e85f7c65374
Prabesh Rai <raiprabesh789@gmail.com>
2024-01-11 17:19:18 -0500

    py: type hint

---
+++ constant/type_hint.py
@@ -0,0 +1,11 @@
+def meow(n: int):
+    for _ in range(n):
+        print("meow")
+
+
+number: int = int(input("Number: "))
+meow(number)
+'''
+mypy - is a static type checker for Pyhton.
+installation - pip install mypy
+'''
