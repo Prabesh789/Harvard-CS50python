0e4f3807b515966b72b7d6e39d5391e34caafe4d
Prabesh Rai <raiprabesh789@gmail.com>
2024-01-15 09:24:51 -0500

    py: list comprehensions

---
+++ unpack/list_compr.py
@@ -0,0 +1,16 @@
+"""
+list comprehensions
+"""
+
+
+def main():
+    yell("This", "is", "CS50")
+
+
+def yell(*words):
+    uppercased = [word.upper() for word in words]
+    print(*uppercased)
+
+
+if __name__ == "__main__":
+    main()
